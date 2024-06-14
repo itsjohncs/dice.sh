3da3cc16f44863ebb6f4c2ab14a677233d18a444
itsjohncs <johnsullivan.pem@gmail.com>
2024-06-14 14:43:42 -0700

    Add skeleton ws-backend. Use vite-node.
    I didn't notice that ts-node's support for esm executables is
    lack-luster. I'm a little confused about what it's getting tripped on
    and I swear I've used it before. But `vite-node` is performing very well
    so I'm happy to switch over.

---
+++ packages/ws-backend/src/main.ts
@@ -0,0 +1,26 @@
+import { WebSocketServer } from "ws";
+
+function getRoomIdFromPath(pathname: string): string | undefined {
+    const pathRe = /^\/room\/([A-Za-z0-9-]+)$/.exec(pathname);
+    if (pathRe) {
+        return pathRe[1];
+    }
+
+    return undefined;
+}
+
+const wss = new WebSocketServer({ port: 45856 });
+
+wss.on("connection", function connection(ws) {
+    const {pathname} = new URL(ws.url, "wss://ws.dice.sh");
+    const roomId = getRoomIdFromPath(pathname);
+
+    ws.on("error", console.error);
+
+    ws.on("message", function message(data) {
+        console.log("Got message", data);
+    });
+
+    ws.send(`Joined ${roomId}`);
+    console.log("Joined", roomId);
+});
