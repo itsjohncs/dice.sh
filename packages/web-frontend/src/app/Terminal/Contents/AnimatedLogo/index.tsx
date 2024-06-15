61c936309c01be651e7515b92f0bb51e387adec0
itsjohncs <johnsullivan.pem@gmail.com>
2024-06-14 20:08:42 -0700

    Stopping point for this direction of the logo.

--- packages/web-frontend/src/app/Terminal/Contents/AnimatedLogo/index.tsx
+++ packages/web-frontend/src/app/Terminal/Contents/AnimatedLogo/index.tsx
@@ -1,4 +1,13 @@
 import styles from "./index.module.css";
+import {
+    HexagonDice,
+    DiceOne,
+    DiceTwo,
+    DiceThree,
+    DiceFour,
+    DiceFive,
+    DiceSix,
+} from "iconoir-react/regular";
 
 function generateRandomASCIIString(numCharacters: number): string {
     const chars =
@@ -24,6 +33,8 @@ export default function AnimatedLogo() {
                 <div className={styles.background}>
                     {generateRandomASCIIString(1000)}
                 </div>
+                <HexagonDice className={styles.d20} width={80} height={80} />
+                <DiceTwo className={styles.d6} width={80} height={80} />
                 <div className={styles.foreground}>&nbsp;DICE.SH&nbsp;</div>
             </div>
         </div>
