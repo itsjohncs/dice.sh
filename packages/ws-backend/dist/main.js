a455f6e6537f1a4411b2ce72ba3229730c1a9549
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-09 16:58:31 -0700

    Rename room to channel.

--- packages/ws-backend/dist/main.js
+++ packages/ws-backend/dist/main.js
@@ -1,6 +1,6 @@
 import {WebSocketServer} from "ws";
-function getRoomIdFromPath(pathname) {
-    const pathRe = /^\/room\/([A-Za-z0-9-]+)$/.exec(pathname);
+function getChannelIdFromPath(pathname) {
+    const pathRe = /^\/channel\/([A-Za-z0-9-]+)$/.exec(pathname);
     if (pathRe) {
         return pathRe[1];
     }
@@ -9,11 +9,11 @@ function getRoomIdFromPath(pathname) {
 const wss = new WebSocketServer({port: 45856});
 wss.on("connection", function connection(ws) {
     const {pathname} = new URL(ws.url, "wss://ws.dice.sh");
-    const roomId = getRoomIdFromPath(pathname);
+    const channelId = getChannelIdFromPath(pathname);
     ws.on("error", console.error);
     ws.on("message", function message(data) {
         console.log("Got message", data);
     });
-    ws.send(`Joined ${roomId}`);
-    console.log("Joined", roomId);
+    ws.send(`Joined ${channelId}`);
+    console.log("Joined", channelId);
 });
