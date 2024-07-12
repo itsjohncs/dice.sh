b68e9efe2ac1902b54bc31112810ea58728b1e39
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-11 17:40:29 -0700

    Move error handling into a wrapper.

--- packages/ws-backend/src/createServer.ts
+++ packages/ws-backend/src/createServer.ts
@@ -12,7 +12,10 @@ export interface ServerConfiguration {
 }
 
 async function fetchAndSendLogEntries_(socket: Socket): Promise<void> {
-    const logEntries = await fetchLogEntriesAfter(socket.data.lastSeenLog);
+    const logEntries = await fetchLogEntriesAfter(
+        socket.data.channelId,
+        socket.data.lastSeenLog,
+    );
     if (logEntries.length > 0) {
         socket.emit("append", logEntries);
     }
@@ -28,12 +31,46 @@ function fetchAndSendLogEntries(
     });
 }
 
+function wrap<Args extends unknown[], AckMessage>(
+    config: ServerConfiguration,
+    socket: Socket,
+    handler: (
+        ...args: [...Args, (data: ErrorMessage | AckMessage) => void]
+    ) => void,
+) {
+    return function (
+        ...args: [...Args, (data: ErrorMessage | AckMessage) => void]
+    ) {
+        const callback = args[args.length - 1] as (
+            data: ErrorMessage | AckMessage,
+        ) => void;
+        try {
+            handler(...args);
+        } catch (e: unknown) {
+            let kind: ErrorMessage["data"]["kind"];
+            if (e instanceof ZodError) {
+                config.logger.warn("Validation error", e.format());
+                kind = "ValidationError";
+            } else {
+                config.logger.error("Unknown error", e);
+                kind = "UnknownError";
+            }
+            callback({
+                type: "Error",
+                data: {kind},
+            });
+            socket.disconnect();
+        }
+    };
+}
+
 export default function createServer(config: ServerConfiguration): Server {
     const server = new SocketIOServer(config.listen.port) as Server;
 
     server.on("connection", function (socket) {
-        socket.on("initialize", function (rawData, callback) {
-            try {
+        socket.on(
+            "initialize",
+            wrap(config, socket, function (rawData, callback) {
                 if (socket.data.state !== undefined) {
                     throw new Error("Invalid state");
                 }
@@ -47,23 +84,8 @@ export default function createServer(config: ServerConfiguration): Server {
                 callback({type: "Empty"});
 
                 fetchAndSendLogEntries(config, socket);
-            } catch (e: unknown) {
-                let kind: ErrorMessage["data"]["kind"];
-                if (e instanceof ZodError) {
-                    config.logger.warn("Validation error", e.format());
-                    kind = "ValidationError";
-                } else {
-                    config.logger.error("Unknown error", e);
-                    kind = "UnknownError";
-                }
-                callback({
-                    type: "Error",
-                    data: {kind},
-                });
-
-                socket.disconnect();
-            }
-        });
+            }),
+        );
     });
 
     return server;
--- packages/ws-backend/src/db.ts
+++ packages/ws-backend/src/db.ts
@@ -1,6 +1,35 @@
+export type LogEntry = unknown;
+
+const data = new Map<string, LogEntry[]>();
+
 export async function fetchLogEntriesAfter(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    lastSeenLog: number,
-): Promise<unknown[]> {
-    return [];
+    channelId: string,
+    lastSeenLog: number | undefined,
+): Promise<LogEntry[]> {
+    const entries = data.get(channelId) ?? [];
+
+    if (entries.length === 0 && lastSeenLog !== undefined) {
+        throw new Error(
+            `Expected lastSeenLog to be undefined, got ${lastSeenLog}.`,
+        );
+    }
+
+    if (entries.length <= lastSeenLog) {
+        throw new Error(
+            `lastSeenLog (${lastSeenLog}) out of bounds (must be less than ${entries.length}).`,
+        );
+    }
+
+    if (lastSeenLog !== undefined) {
+        return entries.slice(lastSeenLog + 1);
+    } else {
+        return entries;
+    }
+}
+
+export async function appendLogEntry(
+    channelId: string,
+    logEntry: LogEntry,
+): Promise<void> {
+    data.get(channelId)?.push(logEntry);
 }
