260e2177a49c2225089c1c49d7cedc07bbc09eb5
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-15 13:45:42 -0700

    Allow unused imports to be auto-fixed by linter.

--- packages/eslint-config/src/base.ts
+++ packages/eslint-config/src/base.ts
@@ -1,12 +1,18 @@
 import eslint from "@eslint/js";
+import unusedImports from "eslint-plugin-unused-imports";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     {
+        plugins: {
+            "unused-imports": unusedImports,
+        },
         rules: {
-            "@typescript-eslint/no-unused-vars": [
+            "@typescript-eslint/no-unused-vars": "off",
+            "unused-imports/no-unused-imports": "error",
+            "unused-imports/no-unused-vars": [
                 "error",
                 {
                     args: "all",
--- packages/eslint-config/src/plugins.d.ts
+++ packages/eslint-config/src/plugins.d.ts
@@ -2,3 +2,8 @@ declare module "eslint-plugin-react/configs/recommended" {
     const foo: import("eslint").Linter.FlatConfig;
     export default foo;
 }
+
+declare module "eslint-plugin-unused-imports" {
+    const foo: import("eslint").ESLint.Plugin;
+    export default foo;
+}
